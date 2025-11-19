636c2b1efe75df0aa691812ea5c972f2ae08f343
Navdeep Singh Chander <49695189+nchand02@users.noreply.github.com>
2025-11-19 14:50:57 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yourusername.github.io',
-  base: '/AI_Tutorial_For_Everyone',
+  site: 'https://nchand02.github.io',
+  base: '/AIConceptsHub',
   build: {
     assets: 'assets'
   },
